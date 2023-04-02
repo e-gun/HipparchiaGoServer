ca91fdac9b8ff01ffdf7111a6f18bd27bb132e62
e-gun <egun@antisigma.classics.utoronto.ca>
2023-04-02 10:24:25 -0400

    trim the number of devel branches

--- theconstants.go
+++ theconstants.go
@@ -10,7 +10,7 @@ import "time"
 const (
 	MYNAME                   = "Hipparchia Golang Server"
 	SHORTNAME                = "HGS"
-	VERSION                  = "1.2.2"
+	VERSION                  = "1.2.3"
 	AVGWORDSPERLINE          = 8 // hard coding a suspect assumption
 	BLACKANDWHITE            = false
 	CHARSPERLINE             = 60 // used by vector to preallocate memory: set it closer to a max than a real average
