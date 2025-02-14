0e27bc4d52482c0e0b36ccf0da1f785ee5085bcd
e-gun <egun@antisigma.classics.utoronto.ca>
2025-02-14 14:50:23 -0500

    modifications to DARKCOLORS

--- web/rt-embhcss.go
+++ web/rt-embhcss.go
@@ -96,6 +96,53 @@ const (
 	--white: hsla(0, 0%, 100%, 1);`
 
 	DARKCOLORS = `
+	--main-body-color: hsla(60,17%,11%, 1);
+	--main-font-color: hsla(60,30%,97%, 1);
+	--input-border-color: hsla(60,17%, 22%, 1);
+	
+	--buttoncolor: hsla(0, 0%, 20%, 1);
+	--button-hover: hsla(187, 23%, 24%, 1);
+	
+	--fieldset-background: hsla(0, 0%, 2%, 1);
+	--focus-shadow: rgba(0, 0, 0, 0.5);
+	--icons-color: rgba(255, 255, 255, 0.54);
+	
+	--black: hsla(0, 0%, 95%, 1);
+	--blue: hsl(167, 35%, 77%);  /* pea green... */
+	--brown: hsla(22, 22%, 74%, 1);
+	--brtblue: hsla(236, 44%, 85%, 1);
+	--copper: hsla(11, 53%, 83%);
+	--deepblue: hsla(57, 65%, 83%, 1);  /* yellow... */
+	--dkbabyblue:  hsla(64, 52%, 84%, 1);  /* yellow... */
+	--dkgreen: hsla(120, 80%, 80%, 1);
+	--dkgrey: hsla(0, 0%, 82%, 1);
+	--dkteal: hsla(203, 22%, 82%, 1);
+	--huedgrey: hsl(113, 35%, 85%);
+	--invisible: hsla(0, 100%, 0%, 0);
+	--lessoffwhite: hsla(0, 0%, 2%, 1);
+	--ltbabyblue: hsla(200, 33%, 15%, 1);
+	--ltgrey: hsla(0, 0%, 20%, 1);
+	--ltgrey2: hsl(206, 7%, 11%);
+	--midgrey: hsla(0, 0%, 53%, 1);
+	--offwhite: hsla(0, 0%, 1%, 1);
+	--orange: hsla(47, 100%, 75%, 1);
+	--pink: hsla(0, 33%, 15%, 1);
+	--pinker: hsl(0, 73%, 20%);
+	--plum: hsla(291, 15%, 72%);
+	--pukegreen: hsl(71, 95%, 78%);
+	--red: hsl(186, 32%, 65%); /* teal... */
+	--dkgreen: hsla(120, 80%, 80%, 1);
+	--rustedorange: hsla(223, 10%, 76%, 1); /* blue-grey... */
+	--sicklyyellow: hsl(45, 16%, 67%);
+	--skyblue: hsla(205, 92%, 73%, 1);
+	--teal: hsla(196, 14%, 71%, 1);
+	--transparentgrey: hsla(0, 0%, 10%, .7);
+	--vdkteal: hsla(196, 27%, 80%, 1);
+	--vdkgrey: hsla(0, 0%, 80%, 1);
+	--vltgrey: hsla(0, 0%, 4%, 1);
+	--white: hsla(0, 0%, 0%, 1);`
+
+	DARKCOLORS_ORIGINAL = `
 	--main-body-color: hsla(0, 0%, 10%, 1);
 	--main-font-color: hsla(0, 0%, 95%, 1);
 	--input-border-color: hsla(0, 0%, 20%, 1);
