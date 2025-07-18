56cbca69c81a6ed279a6c722684caa0d69692ce5
e-gun <egun@antisigma.classics.utoronto.ca>
2025-07-18 09:19:29 -0400

    more fussing with js

--- internal/vv/hltmandjsconst.go
+++ internal/vv/hltmandjsconst.go
@@ -39,75 +39,12 @@ const (
         });
     }`
 
-	// CLICKTOLOOKUPINPROGRESS - todo: replace jquery $( '#lexicadialogtext' ).dialog():
-	CLICKTOLOOKUPINPROGRESS = `
-    var vocabList = document.querySelectorAll('**REPLACEME**');
-    for (let i = 0; i < vocabList.length; i++) {
-        const theitem = vocabList[i];
-        theitem.addEventListener('click', function(e) {
-            const theword = e.target.id.split('--')[0];
-            e.preventDefault();
-            // Get the window dimensions
-           const windowWidth = window.innerWidth;
-           const windowHeight = window.innerHeight;
-
-           // Create a new dialog box element
-           const dialogBox = document.createElement('div');
-           dialogBox.id = 'lexmodalbody';
-           dialogBox.style.width = (windowWidth * 0.33).toString() + 'px';
-           dialogBox.style.height = (windowHeight * 0.9).toString() + 'px';
-           dialogBox.style.position = 'absolute';
-
-           // Set the title of the dialog box
-           const title = document.createElement('h3');
-           title.textContent = this.id;
-           dialogBox.appendChild(title);
-
-           // Make the dialog box draggable
-           let isDragging = false;
-           dialogBox.addEventListener('mousedown', function(e) {
-               isDragging = true;
-           });
-           document.addEventListener('mouseup', function(e) {
-               isDragging = false;
-           });
-           dialogBox.addEventListener('mousemove', function(e) {
-               if (isDragging) {
-                   const x = e.clientX - dialogBox.offsetWidth / 2;
-                   const y = e.clientY - dialogBox.offsetHeight / 2;
-                   dialogBox.style.left = x.toString()+ 'px';
-                   dialogBox.style.top = y.toString()+ 'px';
-               }
-           });
-
-           // Close the dialog box when the user clicks on the close button
-           const closeButton = document.createElement('button');
-           closeButton.textContent = 'Close';
-           closeButton.addEventListener('click', function(e) {
-               dialogBox.remove();
-           });
-           dialogBox.appendChild(closeButton);
-
-           // Add the dialog box to the document
-           document.body.appendChild(dialogBox);
-            // Make a GET request to the /lex/findbyform endpoint
-            fetch('/lex/findbyform/' + theword)
-                .then(response => response.json())
-                .then(definitionreturned => {
-                    // Update the content of the dialog box with the returned data
-                    const newJS = definitionreturned['newjs'];
-                    const newHTML = definitionreturned['newhtml'];
-                    document.getElementById('lexmodalbody').innerHTML = newHTML;
-                    document.getElementById('lexmodal').style.display = 'block';
-                });
-        });
-    }`
-
-	// OLDCLICKTOBROWSE - uses more memory
-	OLDCLICKTOBROWSE = `
+	// CLICKTOBROWSE - in Firefox an index to all of Ovid consumes 960MB of memory: 1.6M objects and 1.1M domNode
+	// the older jQuery version of vv.CLICKTOBROWSE: 1.5GB of memory: 3.2M objects and 3.2M domNode; but there are big
+	// potential problems here: you need to remove the eventlisteners and you cannot remove anonymous handlers ...
+	CLICKTOBROWSE = `
 	$('#pollingdata').hide();
-
-	var indexedLocations = document.querySelectorAll('%s');
+    var indexedLocations = document.querySelectorAll('%s');
 	for (let i = 0; i < indexedLocations.length; i++) {
 		const location = indexedLocations[i];
 		location.addEventListener('click', function(e) {
@@ -116,60 +53,14 @@ const (
 			// example: <td class="passages"><indexedlocation id="index/tlg0612/001/4270--31">11.11.1</indexedlocation></td>
 			// indexedlocation[i].id will be "index/tlg0612/001/4270--31""
 			// but you need to turn "index/tlg0612/001/4270--31" into "index/tlg0612/001/4270" to do the lookup
-			// the "--31" is added to keep the ids distinct and it means "I was assigned by the 31st word in the index"
+			// the "--31" is added to keep the ids unique and it means "I was assigned by the 31st word in the index"
 	
 			const locus = location.id.split('--')[0];
-			$.getJSON('/browse/'+locus, function (passagereturned) {
-				$('#browseforward').unbind('click');
-				$('#browseback').unbind('click');
-				var fb = parsepassagereturned(passagereturned)
-				// left and right arrow keys
-				$('#browserdialogtext').keydown(function(e) {
-					switch(e.which) {
-						case 37: browseuponclick(fb[1]); break;
-						case 39: browseuponclick(fb[0]); break;
-					}
-				});
-				$('#browseforward').bind('click', function(){ browseuponclick(fb[0]); });
-				$('#browseback').bind('click', function(){ browseuponclick(fb[1]); });
-			});
+
+			// see 'browser.js' for the next function
+			browseuponclick(locus);
 		});
-    }`
-
-	// CLICKTOBROWSE - in Firefox an index to all of Ovid consumes 960MB of memory: 1.6M objects and 1.1M domNode
-	// the older jQuery version of vv.CLICKTOBROWSE: 1.5GB of memory: 3.2M objects and 3.2M domNode; but there are big
-	// potential problems here: you need to remove the eventlisteners and you cannot remove anonymous handlers ...
-	CLICKTOBROWSE = `
-	$('#pollingdata').hide();
-
-    var indexedLocations = document.querySelectorAll('%s');
-    for (let i = 0; i < indexedLocations.length; i++) {
-        const location = indexedLocations[i];
-        location.addEventListener('click', function(e) {
-            // Extract the ID from the clicked element's ID attribute
-
-            // example: <td class="passages"><indexedlocation id="index/tlg0612/001/4270--31">11.11.1</indexedlocation></td>
-            // indexedlocation[i].id will be "index/tlg0612/001/4270--31""
-            // but you need to turn "index/tlg0612/001/4270--31" into "index/tlg0612/001/4270" to do the lookup
-            // the "--31" is added to keep the ids distinct and it means "I was assigned by the 31st word in the index"
-
-            const locus = location.id.split('--')[0]; 
-            fetch('/browse/' + locus)
-                .then(response => response.json())
-                .then(passagereturned => {
-                    const fb = parsepassagereturned(passagereturned);
-                    // left and right arrow keys
-                    document.getElementById('browserdialogtext').addEventListener('keydown', e => {
-                        switch (e.which) {
-                            case 37: browseuponclick(fb[1]); break;
-                            case 39: browseuponclick(fb[0]); break;
-                        }
-                    });
-                    document.getElementById('browseforward').addEventListener('click', clickandbrowseforward(fb[0]));
-                    document.getElementById('browseback').addEventListener('click', clickandbrowseback(fb[1]));
-                })
-        });
-    }`
+	}`
 
 	MORPHJS = `
 	<script>
