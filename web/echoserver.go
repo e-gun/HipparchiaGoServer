145ab75570cbb09ecbc4fb76d15dbb8912ac464d
e-gun <egun@antisigma.classics.utoronto.ca>
2026-02-06 10:45:26 -0500

    fiddle with log formats

--- web/echoserver.go
+++ web/echoserver.go
@@ -76,17 +76,22 @@ func StartEchoServer() {
 	}
 
 	lvl2 := func(c *echo.Context, v middleware.RequestLoggerValues) error {
-		// 2026-02-02 17:40:08 INF 200 IP=127.0.0.1:63190 URI=/emb/jq/jquery.min.js
+		// 2026-02-02 17:40:08 INF 200 IP=127.0.0.1 URI=/emb/jq/jquery.min.js
+		ipsplit := strings.Split(c.Request().RemoteAddr, ":")
+		if len(ipsplit) != 2 {
+			return c.String(http.StatusForbidden, "IP format error")
+		}
+
 		if v.Status < 400 {
 			logger.Info().
 				Timestamp().
-				Str("IP", c.Request().RemoteAddr).
+				Str("IP", ipsplit[0]).
 				Str("URI", v.URI).
 				Msg(fmt.Sprintf("%d", v.Status))
 		} else {
 			logger.Warn().
 				Timestamp().
-				Str("IP", c.Request().RemoteAddr).
+				Str("IP", ipsplit[0]).
 				Str("URI", v.URI).
 				Msg(fmt.Sprintf("%d", v.Status))
 		}
@@ -94,7 +99,12 @@ func StartEchoServer() {
 	}
 
 	lvl3 := func(c *echo.Context, v middleware.RequestLoggerValues) error {
-		// 2026-02-02 17:40:08 INF 200 IP=127.0.0.1:64246 SZ=109 UA=Firefox/147.0 URI=/selection/fetch
+		// 2026-02-02 17:40:08 INF 200 IP=127.0.0.1 SZ=109 UA=Firefox/147.0 URI=/selection/fetch
+		ipsplit := strings.Split(c.Request().RemoteAddr, ":")
+		if len(ipsplit) != 2 {
+			return c.String(http.StatusForbidden, "IP format error")
+		}
+
 		ua := strings.Split(v.UserAgent, " ")
 		agent := ua[len(ua)-1]
 
@@ -102,7 +112,7 @@ func StartEchoServer() {
 			logger.Info().
 				Timestamp().
 				Str("SZ", fmt.Sprintf("%d", v.ResponseSize)).
-				Str("IP", c.Request().RemoteAddr).
+				Str("IP", ipsplit[0]).
 				Str("URI", v.URI).
 				Str("UA", agent).
 				Msg(fmt.Sprintf("%d", v.Status))
@@ -110,7 +120,7 @@ func StartEchoServer() {
 			logger.Warn().
 				Timestamp().
 				Str("SZ", fmt.Sprintf("%d", v.ResponseSize)).
-				Str("IP", c.Request().RemoteAddr).
+				Str("IP", ipsplit[0]).
 				Str("URI", v.URI).
 				Str("UA", agent).
 				Msg(fmt.Sprintf("%d", v.Status))
